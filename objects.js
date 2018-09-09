6f598a6a8fef30b180bc5615b61009dd62c85ac6
Paul Ashour <paul.ashour@gmail.com>
2018-09-09 22:09:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -7,6 +7,6 @@ function updatePlaylist(obj, name, title) {
 }
 
 function removeFromPlaylist(obj, name) {
-  return Object.delete(Object.assign( {}, obj ), name);
+  return delete Object.assign( {}, obj ).name;
 }
 
